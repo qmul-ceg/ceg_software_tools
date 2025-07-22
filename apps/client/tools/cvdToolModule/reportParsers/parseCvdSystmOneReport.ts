d0e7ef64516040b1b28fe81afc7731ea4752d055
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-07-22 15:43:39 +0100

    Working on parsing CVD report

--- apps/client/tools/cvdToolModule/reportParsers/parseCvdSystmOneReport.ts
+++ apps/client/tools/cvdToolModule/reportParsers/parseCvdSystmOneReport.ts
@@ -1,4 +1,5 @@
 import { ParserResultInterface } from "@/types/shared.types"
+import Papa from 'papaparse'
 
 export default async function parseCvdSystmOneReport (files:FileList ){
    // const parserResult: ParserResultInterface = {
@@ -54,33 +55,126 @@ export default async function parseCvdSystmOneReport (files:FileList ){
       return sortedReports
    }
 
-   const sortedFiles =  Object.values(await sortFiles(filesArray))
+   const sortedFiles =  await sortFiles(filesArray)
 
-   const parseFiles = async(files: Array<File>): Promise<ParserResultInterface> => {
+
+
+
+
+
+
+
+
+
+   const parseFiles = async(files: Object): Promise<ParserResultInterface> => {
       const parserResult: ParserResultInterface = {
          status : "",
          info : "",
          masterReport: {}
       };
+      
+      const parsedFilesPromises: Array<Promise<object>> = []
 
       
+      let fileArray: Array<File> = []
+      //Get Fix report 3 header 
+      for(const [key, value] of Object.entries(files)){      
+            fileArray.push(value)
+      }
+
+      // console.log(fileArray)
+
+      //Parse each file 
+      const parseFile = async(file: File) => {
+
+         
+
+         return new Promise<Object> ((resolve, reject)=> {
+            let reportObject: Record<string, object> = {}
+
+
+            Papa.parse(file, {
+               header: false,
+               skipEmptyLines:true,
+               complete: (results) => {
+                  const parsedResult = results.data
+                  let parsedResultHeader = parsedResult[0] as Array<string>
+
+                  if (parsedResultHeader.includes('Date of issue')){
+                     for (let i = 0; i < parsedResultHeader.length; i++){
+                        if(parsedResultHeader[i] === 'Date of issue'){
+                           parsedResultHeader[i] = `${parsedResultHeader[i-1].trim()} - Date of issue`;
+                           
+                        }
+                     }
+                  }
+
+                 
+                  for(let i = 1; i < parsedResult.length; i++){
+                     let arrayObject: Record<string, string> = {}
+                     let parsedResultArray = parsedResult[i] as Array<string>
+                     let keysArray = parsedResult[0] as Array<string>
+                     
+                     
+                     for (let j = 0; j < parsedResultArray.length; j++){
+                        arrayObject[keysArray[j]] = parsedResultArray[j]
+                        
+                     } 
+
+                     if (keysArray[i] === "NHS number"){
+                        reportObject[keysArray[i]] = arrayObject
+                        console.log(reportObject)
+                        // console.log(arrayObject)
+                     }
+
+                     
+                     
+                     
+                     
+                  }
+                  
+                  return
+                  
+                  
+                  
+                  // console.log(parsedResult)
+
+                  
+
+
+               }
+
+            })
+         })
+      }
+      
+      for(let file of filesArray){
+         console.log(parseFile(file))
+      }
+      
       return parserResult
+      
+     
    }
+
    
-   // console.log(sortedFiles)
+   const parsedFiles = await parseFiles(sortedFiles)
+   console.log(parsedFiles)
 
-
-   // await sortFiles(filesArray).then((result)=>{
-
-   //    // PLUG IN PARSER TO PARSE FILES 
-   //    console.log(Object.values(result))
-
-  
+}  
 
 
 
 
 
 
-   // return parserResult
-}
+
+// const finalReport: Record<string, object> = {}
+
+      // for (const [key, value] of Object.entries(files)){
+      //    if(key === "report1"){
+      //       console.log(value)
+      //    }
+         
+      // }
+      // console.log(parseFile()) 
